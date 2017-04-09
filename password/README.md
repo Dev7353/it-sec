e1a78a48ab20921536c91e4e98c7b80edd784b3a
Kirill Meng <kimeng@htwg-konstanz.de>
2017-04-09 14:41:12 +0200

    add more README stuff
    	- restructure the python file with new parameters
    	- add printHelp method

--- password/README.md
+++ password/README.md
@@ -1,16 +1,37 @@
 # Manual
 
+Project for it-security. Calculation of hashes and brute force method.
+
 ## Create hashes of passwords within a file
 
-_python password.py -h inputfile_
+> _python password.py -e inputfile_
 
 the output file contains the hashes
-***
 
 ## Calculate a possible combinations (Cartesian product)
 
-_python password -l lengthOfPassword -c fileName_
-***
+> _python password.py -c -l level -s size_
 
-## Brute force 
+level 1: a-z <br>
+level 2: level 1 + A-Z <br>
+level 3: level 2 + 0-9 <br>
+
+Number of combinations level^size
+
+## Brute force method to find passwords
+This method uses the passwords from point above and hash every entry and
+compare this hash with the reference from point one.
+
+> _python password.py -b -x filea -y fileb_
+
+## Additional Notes
+Be aware that if you compute combinations for long passwords with level 3,
+then you need to calculate about 62^pswLength combnations. For Example,
+a length of 6 would mean 62^6  ca. 57 Billion combinations. For a length
+of 7 about 3.5 Trillion. Even if you use multithreading or distributed
+machines it would take a long time. Better download a rainbow table.
+
+http://project-rainbowcrack.com/table.htm
+
+Be sure you have enough space on your data disk.
 
--- password/password.py
+++ password/password.py
@@ -5,24 +5,45 @@ import string, itertools
 verbose = False
 def main():
     try:
-        opts, args = getopt.getopt(sys.argv[1:], "h:b:c:l:v", ["hash", "brute-force", "calc", "length"])
+        opts, args = getopt.getopt(sys.argv[1:], "hecl:s:bx:y:", ["help", "encrypt", "calc", "level", "length", "size", "brute force", "filea", "fileb"])
     except getopt.GetoptError as err:
         print(err)
         sys.exit(2)
 
+    calcFlag = False
+    level = 1
     length = 1
-    for o, a in opts:
-        if o == "-v":
-            verbose = True
-        elif o in ("-h", "--hash"):
-            encrypt(a)
-        elif o in ("-l", "--length"):
-            length = int(a)
-        elif o in ("-c", "--calc"):
-            calcCombinations(length, a)
-        elif o in ("-b", "--brute-force"):
-            bruteForce(a)
 
+    bruteForceFlag = False
+    filea = ""
+    fileb = ""
+
+    for o, a in opts:
+        if o == "-h":
+            printHelp()
+            exit(2)
+        elif o in ("-e", "--encrypt"):
+            encrypt(a)
+        elif o in ("-c", "--calc"):
+            calcFlag = True
+        elif o in ("-l", "--level"):
+            level = int(a);
+        elif o in ("-s", "--size"):
+            length = int(a)
+        elif o in ("-b", "--brute-force"):
+            bruteForceFlag = True
+        elif o in ("-x", "--filea"):
+            filea = a
+        elif o in ("-y", "--fileb"):
+            fileb = a
+
+    if calcFlag == True:
+        calcCombinations(length, level)
+
+    if bruteForceFlag == True:
+        assert filea is not ""
+        assert fileb is not ""
+        bruteForce(filea, fileb)
 
 def encrypt(a):
     if verbose == True:
@@ -44,20 +65,30 @@ def encrypt_string(string):
     hash.update(string)
     return hash.hexdigest()
 
-def calcCombinations(length, file):
-     alphabet_low =  string.ascii_lowercase
-     alphabet_high = string.ascii_uppercase
-     digits = string.digits
+def calcCombinations(length, level):
+     alphabet_low = ""
+     alphabet_high = ""
+     digits = ""
+
+     if level >= 1:
+        alphabet_low += string.ascii_lowercase
+
+     if level >= 2:
+        alphabet_high += string.ascii_uppercase
+
+     if level == 3:
+        digits += string.digits
 
      list_of_combinations = itertools.product(alphabet_low+alphabet_high+digits, repeat=length)
-     hashes = open(file, 'a')
+
+     hashes = open("combinations", 'wb')
      for v in list_of_combinations:
 
          target = ""
          for i in range(length):
              target += v[i]
 
-         hashes.write(target + "|" + encrypt_string(target) + "\n")
+         hashes.write(target + "\n")
 
 
      hashes.close()
@@ -66,7 +97,35 @@ def bruteForce(a, b):
     target = ""
     current = ""
 
+    fileS = open(a, 'r')
+    fileD = open(b, 'r')
 
+
+    srcHashes = fileS.readlines()
+    srcPsw = fileD.readlines()
+
+
+    for srcHash in srcHashes:
+        for entry in srcPsw:
+            hash = encrypt_string(entry[:len(entry)-1])
+
+            if hash == srcHash[:len(srcHash)-1]:
+                print("MATCH HASH " + hash)
+                print("PASSWORD IS " + entry)
+                print("")
+
+
+    fileD.close()
+    fileS.close()
+
+def printHelp():
+
+    print("-h \t --help \t \t \t Help.")
+    print("-e \t --encrypt \t \t \t hash the input passwords within a file and redirect to a output file: output.")
+    print("-c \t --calc \t \t \t calculate all possible passwords for given level and length.")
+    print("-l \t --level \t \t \t level 1: a-z \n level 2: level 1 + A-Z \n level 3 = level 2 + 0-9")
+    print("-s \t --size  \t \t \t size of the password.")
+    print("")
 
 if __name__ == "__main__":
     main()
